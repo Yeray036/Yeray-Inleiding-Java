2e034212cc48bc791f2b1a490552fb2e8d6ff225
Yeray <yeray.guzmanpadron@student.rocvf.nl>
2018-09-03 11:31:12 +0200

    Introductie Java, by Yeray

--- src/H4/Tekenen.java
+++ src/H4/Tekenen.java
@@ -4,4 +4,16 @@ import java.awt.*;
 import java.applet.*;
 
 public class Tekenen extends Applet {
+
+    public void init() {
+        setSize(1600, 900);
+        setBackground(Color.WHITE);
+
+    }
+
+    public void paint(Graphics g) {
+        int[] x = {100, 200, 300};
+        int[] y = {300, 300-(int)(Math.sqrt(Math.pow(200, 2)-Math.pow(100, 2))), 300}; //Dit berekend y= 300+300-Wortel van 200*200-100*100
+        g.drawPolygon(x, y, x.length);
+    }
 }
