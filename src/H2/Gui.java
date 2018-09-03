bfa1ee33b8348d6a266bae05f5f26d03ef9619fd
Yeray <yeray.guzmanpadron@student.rocvf.nl>
2018-09-03 09:05:54 +0200

    Introductie Java, by Yeray

---
+++ src/H2/Gui.java
@@ -0,0 +1,17 @@
+package H2;//importeer de benodigde klassen uit de Java bibliotheek
+import java.awt.*;
+import java.applet.*;
+
+//een klasse met de naam Show van het type Applet
+public class Gui extends Applet {
+
+    //een (lege) methode die de Applet initialiseert
+    public void init() {
+
+    }
+    //een methode die de inhoud van het scherm tekent
+    public void paint(Graphics g) {
+        g.drawString("Welcome to Java!!", 50, 60 );
+    }
+
+}
