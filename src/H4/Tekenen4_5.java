e78dd074dc17ee347ec4be70826182ae6d0774fd
Yeray <yeray.guzmanpadron@student.rocvf.nl>
2018-09-03 14:36:23 +0200

    Introductie Java, by Yeray

--- src/H4/Tekenen4_5.java
+++ src/H4/Tekenen4_5.java
@@ -1,4 +1,18 @@
 package H4;
 
-public class Tekenen4_5 {
+import java.awt.*;
+import java.applet.*;
+
+public class Tekenen4_5 extends Applet {
+
+    public void init() {
+        setSize(1600, 900);
+        setBackground(Color.WHITE);
+
+    }
+
+    public void paint(Graphics g) {
+
+
+    }
 }
