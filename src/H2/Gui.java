74d8f53b920f443faccc20f361ebb6777e8056c7
Yeray <yeray.guzmanpadron@student.rocvf.nl>
2018-09-03 10:32:42 +0200

    Introductie Java, by Yeray

--- src/H2/Gui.java
+++ src/H2/Gui.java
@@ -8,13 +8,15 @@ public class Gui extends Applet {
     //een (lege) methode die de Applet initialiseert
     public void init() {
 
-        setBackground(Color.magenta);
+        setBackground(Color.WHITE);
 
     }
     //een methode die de inhoud van het scherm tekent
     public void paint(Graphics g) {
-        g.setColor(Color.YELLOW);
-        g.drawString("Welcome to Java!!", 50, 60 );
+        g.setColor(Color.blue);
+        g.drawString("Yeray", 50, 60 );
+        g.setColor(Color.red);
+        g.drawString("Guzmán Padrón", 50, 70);
     }
 
 
