fbb5734d715668e2fb39e261e8ea219c68b2e0b7
Yeray <yeray.guzmanpadron@student.rocvf.nl>
2018-09-04 13:35:38 +0200

    Introductie Java, by Yeray

---
+++ src/H6/H6_1.java
@@ -0,0 +1,28 @@
+package H6;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class H6_1 extends Applet {
+
+    int Jan;
+    int Ali;
+    int Jeannette;
+    int Yeray;
+    int Verdiend;
+
+    public void init(){
+        Jan = a;
+        Ali = b;
+        Jeannette = c;
+        Yeray = d;
+        Verdiend = 113;
+
+    }
+
+    public void paint(Graphics g){
+        g.drawString("Jan", 100, 100);
+
+    }
+
+}
