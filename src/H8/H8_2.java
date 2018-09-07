c4ae7e2448db917b5b5c223dafc25594ed6c2501
Yeray <yeray.guzmanpadron@student.rocvf.nl>
2018-09-07 09:08:46 +0200

    Introductie Java, by Yeray

--- src/H8/H8_2.java
+++ src/H8/H8_2.java
@@ -2,6 +2,7 @@ package H8;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.*;
 
 public class H8_2 extends Applet {
 
@@ -13,36 +14,92 @@ public class H8_2 extends Applet {
     4: potentiële vrouwen.
      */
 
-    Button man;
-    Button vrouw;
-    Button potentiëleman;
-    Button potentiëlevrouw;
+    int totaal;
+    int man;
+    int vrouw;
+    int pman;
+    int pvrouw;
+    Button knopman;
+    Button knopvrouw;
+    Button knoppman;
+    Button knoppvrouw;
 
-    public void init(){
+    public void init() {
 
         setSize(1600, 900);
 
-        man = new Button();
-        man.setLabel("Mannelijke personen");
+        //knopman
+        knopman = new Button("Mannelijke personen");
+        knopmanListener kml = new knopmanListener();
+        knopman.addActionListener( kml );
+        add(knopman);
 
-        vrouw = new Button();
-        vrouw.setLabel("Vrouwlijke personen");
+        //knopvrouw
+        knopvrouw = new Button("Vrouwlijke personen");
+        knopvrouwListener kvl = new knopvrouwListener();
+        knopvrouw.addActionListener( kvl );
+        add(knopvrouw);
 
-        potentiëleman = new Button();
-        potentiëleman.setLabel("Potentiële mannen");
+        //knop potentiële mannen
+        knoppman = new Button("Potentiële mannen");
+        knoppmanListener kpml = new knoppmanListener();
+        knoppman.addActionListener( kpml );
+        add(knoppman);
 
-        potentiëlevrouw = new Button();
-        potentiëlevrouw.setLabel("Potentiële vrouwen");
-
-        add(man);
-        add(vrouw);
-        add(potentiëleman);
-        add(potentiëlevrouw);
-
+        //knop potentiële vrouwen
+        knoppvrouw = new Button("Potentiële vrouwen");
+        knoppvrouwListener kpvl = new knoppvrouwListener();
+        knoppvrouw.addActionListener( kpvl );
+        add(knoppvrouw);
     }
 
-    public void paint(Graphics g){
+    public void paint(Graphics g) {
+        g.drawString("Mannen : " + man,50,80);
+        g.drawString("Vrouwen : " + vrouw, 50, 120);
+        g.drawString("Potentiële mannen : " + pman, 50, 160);
+        g.drawString("Potentiële vrouwen : " + pvrouw, 50, 200);
+        g.drawString("Totaal : " + totaal, 50, 240);
+    }
 
+    class knopmanListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            man = man + 1;
+            totaal = totaal + 1;
+            repaint();
+        }
+    }
+
+    class knopvrouwListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            vrouw = vrouw + 1;
+            totaal = totaal + 1;
+            repaint();
+        }
+    }
+
+    class knoppmanListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            pman = pman + 1;
+            totaal = totaal + 1;
+            repaint();
+        }
+    }
+
+    class knoppvrouwListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            pvrouw = pvrouw + 1;
+            totaal = totaal + 1;
+            repaint();
+        }
     }
 
 }
+
