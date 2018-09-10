fb34020fb59b0527d97cc5f1d4c5844560bffec2
Yeray <yeray.guzmanpadron@student.rocvf.nl>
2018-09-10 11:19:30 +0200

    Introductie Java, by Yeray

---
+++ src/H8/H8_Praktijk.java
@@ -0,0 +1,16 @@
+package H8;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class H8_Praktijk extends Applet {
+
+    public void init(){
+
+    }
+
+    public void paint(Graphics g){
+        
+    }
+
+}
