9a917951a16a0457fbd6a71b004681946a3cd3de
Yeray <yguzmanpadron@roc-dev.com>
2018-10-01 10:37:45 +0200

    Introductie Java, by Yeray

---
+++ src/H11/H11_4.java
@@ -0,0 +1,24 @@
+package H11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class H11_4 extends Applet {
+
+    int tafelvandrie;
+
+    @Override
+    public void init() {
+        tafelvandrie = 3;
+        for(int i = 1; i <= 10; i++){
+        }
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        tafelvandrie = 3;
+        for (int i = 1; i <= 10; i++) {
+            g.drawString("" + tafelvandrie*i, 50, 50+20*i);
+        }
+    }
+}
