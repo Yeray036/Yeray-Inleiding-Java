bbb7bb2e302c2441c6936ea1224a41842f9b0a70
Yeray <yeray.guzmanpadron@student.rocvf.nl>
2018-10-29 14:34:21 +0100

    Introductie Java, by Yeray

--- src/H14/H14_praktijkopdracht.java
+++ src/H14/H14_praktijkopdracht.java
@@ -10,6 +10,7 @@ public class H14_praktijkopdracht extends Applet {
 
     TextField numberinjection;
     Label label;
+    int numberOfTokens;
 
     private Image token;
     private URL pad;
@@ -17,15 +18,21 @@ public class H14_praktijkopdracht extends Applet {
     @Override
     public void init() {
 
-        pad =  H14_praktijkopdracht.class.getResource("/H14/Images/");
+        setSize(720, 720);
+
+        numberOfTokens = 23;
+
+        pad = H14_praktijkopdracht.class.getResource("/H14/Images/");
 
         label = new Label("Voer één, twee en of drie in");
         add(label);
 
         numberinjection = new TextField("", 8);
         numberinjectionListener n = new numberinjectionListener();
-        numberinjection.addActionListener( n );
+        numberinjection.addActionListener(n);
         add(numberinjection);
+
+        setBackground(Color.black);
 
     }
 
@@ -38,14 +45,27 @@ public class H14_praktijkopdracht extends Applet {
 
         @Override
         public void actionPerformed(ActionEvent e) {
+            int input = Integer.parseInt(numberinjection.getText());
+            if (input < 4 && input > 0) {
+                numberOfTokens = numberOfTokens - input;
+                repaint();
 
+
+            }
         }
     }
 
     public void tokens(Graphics g) {
-        for (int i = 0; i < 24 ; i++) {
+        int x = 0;
+        int y = 5;
+        for (int i = 0; i < numberOfTokens; i++) {
+            if (i % 5 == 0) {
+                x = 50;
+                y += 120;
+            }
             token = getImage(pad, "tokenschips.png");
-            repaint();
+            g.drawImage(token, x, y, 100, 100, this);
+            x += 110;
         }
     }
 }
