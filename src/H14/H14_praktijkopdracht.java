1e1f65fb7f954b50860cdda705186d3f446215af
Yeray <yeray.guzmanpadron@student.rocvf.nl>
2018-10-30 11:06:19 +0100

    Introductie Java, by Yeray

--- src/H14/H14_praktijkopdracht.java
+++ src/H14/H14_praktijkopdracht.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URL;
+import java.util.Random;
 
 public class H14_praktijkopdracht extends Applet {
 
@@ -14,6 +15,8 @@ public class H14_praktijkopdracht extends Applet {
 
     private Image token;
     private URL pad;
+
+    int[] specialnumbers = {1, 5, 9, 13, 17, 21};
 
     @Override
     public void init() {
@@ -25,10 +28,12 @@ public class H14_praktijkopdracht extends Applet {
         pad = H14_praktijkopdracht.class.getResource("/H14/Images/");
 
         label = new Label("Voer één, twee en of drie in");
+        label.setBackground(Color.black);
+        label.setForeground(Color.white);
         add(label);
 
         numberinjection = new TextField("", 8);
-        numberinjectionListener n = new numberinjectionListener();
+        numberInjectionListener n = new numberInjectionListener();
         numberinjection.addActionListener(n);
         add(numberinjection);
 
@@ -38,20 +43,54 @@ public class H14_praktijkopdracht extends Applet {
 
     @Override
     public void paint(Graphics g) {
+        g.setColor(Color.RED);
+        g.drawString("Number of poker chips left : " + numberOfTokens, 50, 50);
         tokens(g);
+        if (numberOfTokens <= 1) {
+
+        }
     }
 
-    class numberinjectionListener implements ActionListener {
-
+    class numberInjectionListener implements ActionListener {
+        boolean checkUserTurn = true;
+        int input;
         @Override
         public void actionPerformed(ActionEvent e) {
-            int input = Integer.parseInt(numberinjection.getText());
+            if (checkUserTurn == false)
+                return;
+            input = Integer.parseInt(numberinjection.getText());
             if (input < 4 && input > 0) {
                 numberOfTokens = numberOfTokens - input;
                 repaint();
-
-
             }
+            checkUserTurn = false;
+            new java.util.Timer().schedule(
+                    new java.util.TimerTask() {
+                        @Override
+                        public void run() {
+                            for (int i : specialnumbers)
+                                if (numberOfTokens - 1 == i)
+                                    input = 1;
+                                else if (numberOfTokens - 2 == i)
+                                    input = 2;
+                                else if (numberOfTokens - 3 == i)
+                                    input = 3;
+                            Random random = new Random();
+                            if (input == 0)
+                                if (numberOfTokens >= 4)
+                                    input = random.nextInt(3)+1;
+                                else
+                                    input = 1;
+
+                            numberOfTokens -= input;
+
+                            repaint();
+                            checkUserTurn = true;
+                        }
+                    },
+                    1000 // Wait 1 second before the PC makes a turn
+            );
+
         }
     }
 
